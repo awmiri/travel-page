b9a4e515a01a3dbcc690669cfbecce3a294e5691
awmiri <talasaz1382@gmail.com>
2025-11-07 17:05:33 +0330

    done header in decstop size also add dark and light mood also add animation in btns and navbars

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Header from "@/template/module/Header";
+import Head from "next/head";
 
 
 export default function Home() {
   return (
-    <div>home</div>
+    <>
+      <Head>
+        <title>سفرکن</title>
+        <meta
+          name="description"
+          content="سفرکن، سامانه هوشمند رزرو بلیط هواپیما، قطار و اتوبوس با بهترین قیمت و پشتیبانی ۲۴ ساعته. سفرت رو سریع و مطمئن شروع کن!"
+        />
+      </Head>
+      <header>
+        <Header />
+      </header>
+    </>
   );
 }
