b13b043140076af1b72ec116b7fad4ff998f4836
awmiri <talasaz1382@gmail.com>
2025-11-08 15:35:04 +0330

    done sign up api also add jwt for the user and set that on the cookies

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,11 +1,12 @@
 import ConnectDb from '@/config/ConnectDb'
 import UserModel from '@/model/user'
-import { hashPassword } from '@/utility/auth'
+import { generateToken, hashPassword } from '@/utility/auth'
+import { serialize } from 'cookie'
 
 
-const signup = async (req, rea) => {
+const signup = async (req, res) => {
     if (req.method !== "POST") {
-        return rea.status(500).json({ message: "method not valid" })
+        return res.status(405).json({ message: "method not valid" })
     }
     await ConnectDb()
 
@@ -13,7 +14,7 @@ const signup = async (req, rea) => {
         const { phone, email, password } = req.body
 
         if (!phone.trim() || !password.trim() || !email.trim()) {
-            return rea.status(400).json({ message: "value are not completely filled blank" })
+            return res.status(400).json({ message: "value are not completely filled blank" })
         }
 
         const user = await UserModel.findOne({
@@ -21,15 +22,24 @@ const signup = async (req, rea) => {
         })
 
         if (user) {
-            return rea.status(409).json({ message: "user are login before whit this information" })
+            return res.status(409).json({ message: "user are login before whit this information" })
         }
 
         const hashPass = await hashPassword(password)
 
         const createUser = await UserModel.create({ phone, email, password: hashPass })
+        const token = generateToken(email)
+
+        return res.setHeader("set-cookie", serialize("token", token, {
+            httpOnly: true,
+            path: "/",
+            maxAge: 60 * 60 * 24 * 2
+        })).status(201).json({ message: "sign up successfully done" })
 
 
     } catch (err) {
+        console.error("Signup error:", err);
+        return res.status(500).json({ message: "server error", error: err.message });
 
     }
 }
--- utility/auth.js
+++ utility/auth.js
@@ -1,8 +1,17 @@
 import { hash } from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 const hashPassword = async (password) => {
     const hashedPassword = await hash(password, 12)
     return hashedPassword
 }
 
-export { hashPassword } 
+const generateToken = (data) => {
+    const token = sign({ ...data }, process.env.privetTokenKey, {
+        algorithm: "HS256",
+        expiresIn: "42h"
+    })
+    return token
+}
+
+export { hashPassword, generateToken } 
