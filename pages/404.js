c3602c7fdc4e88e8e1bfdccf743de6ae78f6f245
awmiri <talasaz1382@gmail.com>
2025-11-08 03:48:52 +0330

    done 404 psge cuse also download lottie react for get animation for 404

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import Link from 'next/link';
+import Header from '@/template/module/Header';
+
+function NotFoundPage() {
+    return (
+        <div>
+            <Header />
+            <div className='flex flex-col items-center justify-center h-dvh -mt-22'>
+                <div className="w-[400px] sm:w-[500px] md:w-[600px] lg:w-[800px]">
+                    <DotLottieReact
+                        src="https://lottie.host/a7a0d4b0-06da-4f81-b4a8-0de7801d1289/Psb96j6rFm.lottie"
+                        loop
+                        autoplay
+                    />
+                </div>
+                <p className='font-iranYekanBold text-base md:text-xl mt-2 text-cusBlue'>متأسفیم! صفحه‌ای که به دنبال آن هستید پیدا نشد.</p>
+                <Link href={"/"} className="button type1 mt-6">
+                    <span className="btn-txt">رفتن به صفحه اصلی</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default NotFoundPage
--- template/module/Header.js
+++ template/module/Header.js
@@ -48,7 +48,7 @@ function Header() {
                             <li className='p-2.5 font-iranYekanRegular text-textLight dark:text-white text-sm'>سفرنامه</li>
                         </ul>
                     </div>
-                    <div className='cursor-pointer' onClick={() => setOpenMobileNav(true)}>
+                    <div className='cursor-pointer sm:hidden' onClick={() => setOpenMobileNav(true)}>
                         <svg viewBox="0 0 24 24" fill="currentColor" className="size-6 text-textLight dark:text-textDark">
                             <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                         </svg>
